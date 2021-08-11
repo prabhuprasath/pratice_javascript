e259bb5510e94c2e0650ceba95961725bc00760d
prabhuprasath <prabhuprasath02@gmail.com>
2021-08-11 20:46:24 +0530

    created variables.js files

---
+++ javascript basics/variables.js
@@ -0,0 +1,16 @@
+//variables - store , access, modify
+//variable creation
+//initialize , declaration
+
+// variables keywords are : var , let , const.
+
+var birthyear = 2001;
+console.log(birthyear);
+
+let age = 20;
+console.log(age);
+
+const favoriteprogramminglanguages = "javascript";
+
+console.log(favoriteprogramminglanguages);
+
