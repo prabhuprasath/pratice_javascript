424f257cf42e733c904fb97c942c5b94f321a714
prabhuprasath <prabhuprasath02@gmail.com>
2021-08-18 00:04:35 +0530

    added loops.js file

---
+++ javascript basics/loops.js
@@ -0,0 +1,28 @@
+//loops
+//while loops
+
+let cgpa = 6.1;
+
+while (cgpa<=10) {
+    console.log(cgpa);
+    cgpa +=2;
+    
+}
+
+//do while
+
+let salary = 20_000;
+
+do{
+    console.log(`your salary is: ${salary}`);
+    salary += 10_000;
+}while(salary<50_000); 
+
+//for loops
+
+let value = 20;
+
+for(value = 20; value <= 30; value +=5){
+    console.log(value);
+
+}
