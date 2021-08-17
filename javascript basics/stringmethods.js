75664a89275e35e2b0681369241ff2f969850356
prabhuprasath <prabhuprasath02@gmail.com>
2021-08-18 00:08:45 +0530

    added stringmethods file

---
+++ javascript basics/stringmethods.js
@@ -0,0 +1,33 @@
+const randomstring = '                 javascript                              ';
+
+console.log(randomstring.length);
+
+let value = randomstring.substring(2,6);
+
+console.log(value);
+
+console.log(randomstring.indexOf('s'));
+
+console.log(randomstring.split(''));
+
+console.log(randomstring.valueOf());
+
+console.log(randomstring.toUpperCase());
+
+console.log(randomstring.trim());
+
+//array properties and array methods
+
+const items = ['cars','bike','cycle'];
+
+const city = ['chennai','chengalpattu','madurai'];
+
+const result = items.concat(city);
+
+
+console.log(result);
+
+console.log(result.reverse());
+
+console.log(result.includes('cycle'));
+
