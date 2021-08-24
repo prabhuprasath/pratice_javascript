8eb15b1a89ba512c447e95c16afb600db3a12cfa
prabhuprasath <prabhuprasath02@gmail.com>
2021-08-24 19:17:49 +0530

    arrowfunctions are quite good

---
+++ javascript basics/Arrowfunctions.js
@@ -0,0 +1,77 @@
+
+const n = () =>{
+        const value = 'arrowfunctions'
+    const store = value.toUpperCase();
+    console.log(`Hello ${store}`);
+
+}
+n();
+
+
+const printzero = (num1,num2) =>{
+
+    return num1 - num2;
+
+}
+const returnAnswer = printzero(5,5);
+console.log(returnAnswer);
+
+
+const check = (a,b) =>{
+    return a * b;
+
+}
+
+const save = check(8,6);
+
+console.log(save);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
