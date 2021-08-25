8b8b6fd24b7616928adbcdbc3e3594ba752dcaee
prabhuprasath <prabhuprasath02@gmail.com>
2021-08-25 19:17:03 +0530

    browserstorage.js is working fine

---
+++ javascript basics/browserstorage.js
@@ -0,0 +1,12 @@
+localStorage.setItem('bike','yamaha');
+localStorage.setItem('car','hyundai certa');
+localStorage.setItem('cycle','waltx');
+
+sessionStorage.setItem('name1','peter');
+sessionStorage.setItem('name2','sussy');
+sessionStorage.setItem('name3','bob');
+
+const store = localStorage.getItem('car');
+alert(store);
+
+console.log(store);
